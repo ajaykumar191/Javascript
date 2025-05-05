47973039a697a0f9bb6467a5bf0a8711a837c4ef
ajay12aa <122091340+ajay12aa@users.noreply.github.com>
2025-05-05 11:51:33 +0530

    Added modules

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,9 +1,8 @@
+import { cart,addToCart } from "../data/cart.js";
+import { products } from "../data/products.js"
 
 let productsHtml = '';
-
 products.forEach((product, index) => {
-
-  //images/products/black-2-slot-toaster.jpg
 
   productsHtml += ` <div class="product-container">
         <div class="product-image-container">
@@ -58,33 +57,24 @@ products.forEach((product, index) => {
 document.querySelector(".js-products-grid").innerHTML = productsHtml;
 
 
+ function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+
+    cartQuantity += cartItem.quantity;
+  })
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
 
 
 document.querySelectorAll(".js-add-to-cart").forEach((button, index) => {
   button.addEventListener("click", () => {
 
     const productId = (button.dataset.productId);
+    addToCart(productId);
+    updateCartQuantity();
 
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    })
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      })
-    }
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-
-      cartQuantity += item.quantity;
-    })
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 })
 
