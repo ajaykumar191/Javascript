34f676bebe8e67239b614aade18651e9e70a5d67
ajay12aa <122091340+ajay12aa@users.noreply.github.com>
2025-05-10 11:06:14 +0530

    Promises

--- javascript-amazon-project/data/backend-practice.js
+++ javascript-amazon-project/data/backend-practice.js
@@ -1,7 +1,7 @@
-const xhr = new XMLHttpRequest();
-xhr.addEventListener('load', () => {
-    console.log(xhr.responseText);
+// const xhr = new XMLHttpRequest();
+// xhr.addEventListener('load', () => {
+//     console.log(xhr.responseText);
 
-});
-xhr.open('GET', 'https://supersimplebackend.dev');
-xhr.send();
+// });
+// xhr.open('GET', 'https://supersimplebackend.dev');
+// xhr.send();
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -3,8 +3,59 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 import '../data/backend-practice.js'
 
-loadProducts(() => {
+import { loadCart } from "../data/cart.js";
+
+Promise.all([
+    new Promise((resolve) => {
+        console.log('start');
+
+        loadProducts(() => {
+            console.log('finished loading');
+
+            resolve();
+        })
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+
+        });
+    })
+
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 })
 
+
+// const promise = new Promise((resolve) => {
+//     console.log('start');
+
+//     loadProducts(() => {
+//         console.log('finished loading');
+
+//         resolve();
+//     })
+
+// }).then(() => {
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+
+//         });
+//     })
+
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+
+// loadProducts(() => {
+//     loadCart(() => {
+
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     })
+
+// }); 
